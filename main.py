1eb54d804dbff9f9b559864a8690d06e424851e9
lee-hyeon-gu <149756412+lee-hyeon-gu@users.noreply.github.com>
2024-04-30 10:34:12 +0900

    init

---
+++ main.py
@@ -0,0 +1,127 @@
+import uvicorn
+from typing_extensions import Annotated
+from fastapi.staticfiles import StaticFiles
+import torchvision.transforms as transforms
+from PIL import Image
+import torch
+from fastapi import FastAPI, Request, File, UploadFile ,Form
+from fastapi.templating import Jinja2Templates
+import os
+
+
+app = FastAPI()
+
+app.mount('/static',StaticFiles(directory='static'),name='static')
+templates=Jinja2Templates(directory='templates')
+
+
+@app.get('/')
+def test_get(request:Request):
+    return templates.TemplateResponse('post_text.html',{'request':request})
+
+@app.post('/')
+async def uploader_file(request: Request,file: UploadFile = File(...)):
+    content = await file.read() #비동기처리, 해당 코드의 연산이 끝날때 까지 코드의 실행이 멈추지 않고 다음 코드 실행
+    file_save_folder = './'
+    with open(os.path.join(file_save_folder, file.filename), "wb") as fp:
+        fp.write(content)
+    print('업로드 완료')
+    # output = infer(file_save_folder+file.filename)
+    return templates.TemplateResponse("result_page.html", {'request': request})#,'result':output})
+
+# @app.post('/result_page')
+# def test_get(request:Request):
+#     return templates.TemplateResponse('result_page.html',{'request':request})
+
+
+@app.post('/list')
+def mysqltest(request:Request):
+    query = db_connection.execute('select * from videos')
+    result_db = query.fetchall()
+        
+    result=[]
+    for data in result_db:
+        temp = {'vid_name':data[0],'vid_tag':data[-1]}
+        result.append(temp)
+    print(result)
+    return templates.TemplateResponse('list.html',{'request':request,'result_table':result})
+
+from sqlalchemy import create_engine
+db_connection = create_engine('mysql://root:1234@127.0.0.1:3306/test')
+
+@app.get('/list')
+def mysqltest(request:Request):
+    query = db_connection.execute('select * from videos')
+    result_db = query.fetchall()
+        
+    result=[]
+    for data in result_db:
+        temp = {'vid_name':data[0],'vid_tag':data[-1]}
+        result.append(temp)
+    print(result)
+    return templates.TemplateResponse('list.html',{'request':request,'result_table':result})
+
+
+
+    
+    
+
+@app.get('/detail')
+def test_post(request:Request,vid_name:str):
+    print('vid',vid_name)
+    query = db_connection.execute("select * from videos where vid_name = '{}'".format(vid_name))
+    result_db = query.fetchall()
+    
+    result = []
+    for data in result_db:
+        temp = {'vid_name':data[0], 'vid_url' : data[1] , 'vid_fake' :data[2],'vid_tag' : data[-1]}
+        result.append(temp)
+    return templates.TemplateResponse('detail.html',{'request':request,'result_table':result})
+
+
+from fastapi import Request, Form
+from sqlalchemy import text
+
+@app.post('/update')
+def post_update(request: Request, vid_name: str,vid_tag:Annotated[str,Form()]):
+
+    if vid_name:
+        update_query = text("UPDATE videos SET vid_tag ='{}' WHERE  vid_name ='{}'".format(vid_tag,vid_name))
+        db_connection.execute(update_query, {"vid_name": vid_name, "vid_tag": vid_tag})
+    
+    select_query = text("SELECT * FROM videos WHERE vid_name ='{}' ".format(vid_name))
+    result_db = db_connection.execute(select_query, {"vid_name": vid_name}).fetchall()
+
+    result = []
+    for row in result_db:
+        temp = {'vid_name': row[0], 'vid_url': row[1], 'vid_fake': row[-2], 'vid_tag': row[-1]}
+        result.append(temp)
+
+    return templates.TemplateResponse('detail.html', {'request': request, 'result_table': result})
+
+
+# @app.post('/update')
+# def add_tag(request: Request, vid_name: str, additional_tag: str = Form(...)):
+
+#     if vid_name and additional_tag:
+#         # 기존 vid_tag에 새로운 값을 덧붙이는 쿼리
+#         update_query = text("UPDATE videos SET vid_tag = CONCAT(vid_tag, :additional_tag) WHERE vid_name = :vid_name")
+#         db_connection.execute(update_query, {"additional_tag": additional_tag, "vid_name": vid_name})
+    
+#     select_query = text("SELECT * FROM videos WHERE vid_name = :vid_name")
+#     result_db = db_connection.execute(select_query, {"vid_name": vid_name}).fetchall()
+
+#     result = []
+#     for row in result_db:
+#         temp = {'vid_name': row[0], 'vid_url': row[1], 'vid_fake': row[-2], 'vid_tag': row[-1]}
+#         result.append(temp)
+
+#     return templates.TemplateResponse('detail.html', {'request': request, 'result_table': result})
+
+
+
+
+
+
+if __name__ =='__main__':
+    uvicorn.run(app,host='localhost',port = 8001)
